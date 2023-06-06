358859e129740d049c3070ed64f8ecc76d76aba0
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-06 12:39:44 +0100

    Created MainHeader.vue

---
+++ src/components/Header/MainHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li>
+        Level 1
+    </li>
+    <li>
+        Level 2
+    </li>
+    <li>
+        Level 3
+    </li>
+    <li>
+        Contact
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
