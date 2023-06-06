2a371e374e7acd29ba00b59ae0c883319dc6d3fd
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-06 12:10:31 +0100

    Updated MainFooter.vue

--- src/components/Footer/MainFooter.vue
+++ src/components/Footer/MainFooter.vue
@@ -4,3 +4,51 @@
     <a href="https://github.com/Lu-Smith/memory_game_vue" target="_blank">GitHub</a>.</footer>
 </template>
 
+<style>
+  footer {
+      position: fixed;
+      bottom: 0;
+      right: 0;
+      left: 0;
+      text-align: center;
+      padding: 20px;
+      color: rgb(250, 250, 250);
+      background: black;
+  }
+  
+  footer a {
+    color: rgb(215, 241, 223);
+    text-decoration: none;
+    position: relative;
+    transition: all 0.3s ease-in-out;
+  }
+  
+  footer a::before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    bottom: -5px;
+    left: 0;
+    background-color: #9fd3c7;
+    visibility: hidden;
+    transform: scaleX(0);
+    transition: all 0.3s ease-in-out;
+  }
+  
+  footer a:hover {
+    color: #9fd3c7;
+  }
+  
+  footer a:hover::before {
+    visibility: visible;
+    transform: scaleX(1);
+  }
+
+  @media screen and (max-width: 480px) {
+    footer {
+      padding: 10px;
+      font-size: 12px;
+    }
+}
+</style>
