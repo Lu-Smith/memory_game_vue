740698eadd80bf9f5d012cbe2c0b37a435a95fac
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-06 11:58:34 +0100

    Created MainFooter.vue

---
+++ src/components/Footer/MainFooter.vue
@@ -0,0 +1,6 @@
+<template>
+    <DateFooter />
+    <footer>Coded by <a href="https://www.lunasmithart.com/" target="_blank">Luna Smith</a>, open-sourced on 
+    <a href="https://github.com/Lu-Smith/memory_game_vue" target="_blank">GitHub</a>.</footer>
+</template>
+
