44204a399bedd0771fbcd9420ba470377b867302
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-06 12:50:28 +0100

    Updated MainHeader.vue

--- src/components/Header/MainHeader.vue
+++ src/components/Header/MainHeader.vue
@@ -9,7 +9,7 @@
     <li>
         Level 3
     </li>
-    <li>
+    <li class="contactLink">
         Contact
     </li>
   </ul>
@@ -22,5 +22,34 @@ export default {
 </script>
 
 <style>
+ul {
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    padding-top: 10px;
+}
+
+li {
+    padding: 10px 15px;
+    border-radius: 5px;
+    transition: all 200ms ease;
+    font-weight: 800;
+}
+
+li:hover {
+    background: #9fd3c7;
+    box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.2);
+}
+
+.contactLink {
+    background: #9fd3c7;
+}
+
+.contactLink:hover {
+    background: #57746d;
+    color: white;
+}
 
 </style>
