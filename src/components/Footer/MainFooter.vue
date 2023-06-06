51f99e35d057c476b790ae27c596854a168fbcac
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-06 12:19:35 +0100

    Updated GameTimer.vue

--- src/components/Footer/MainFooter.vue
+++ src/components/Footer/MainFooter.vue
@@ -1,5 +1,4 @@
 <template>
-    <DateFooter />
     <footer>Coded by <a href="https://www.lunasmithart.com/" target="_blank">Luna Smith</a>, open-sourced on 
     <a href="https://github.com/Lu-Smith/memory_game_vue" target="_blank">GitHub</a>.</footer>
 </template>
--- src/components/Header/GameTimer.vue
+++ src/components/Header/GameTimer.vue
@@ -1,5 +1,7 @@
 <template>
-    <h3>I am a timer</h3>
+    <div class="timer">
+        <h3>I am a timer</h3>
+    </div>
 </template>
 
 <script lang="ts">
@@ -9,5 +11,13 @@ export default {
 </script>
 
 <style>
+.timer {
+    background: black;
+    color: #9fd3c7;
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 100dvw;
+}
 
 </style>
